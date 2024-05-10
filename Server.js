c15a9b728421e452da961b9fc74150fe6c8f3258
Steven Burrell <sburrell23@gmail.com>
2024-05-09 19:54:02 -0600

    tom cruise, tom hanks and elephants are way too heavily weighted. gameplay loop is less enjoyable then expected.

--- Server.js
+++ Server.js
@@ -37,9 +37,9 @@ async function callTriviaAI(req, res, category) {
                 role: "system",
                 content: `
                 The response you give will be in json. 
-                The emojis and your reasons should be an array of objects called 'clues'. An obejct will look like this: {emoji: '😀', reason: 'This ${category} is usually happy.'}
-                The name of the random ${category} you chose should be a string called 'answer' that live as it own key at the root of the object.
-                Here is the structure of the object {clues:[{emoji:'',reason:''}],answer:''}
+                The emojis and your reasons should be an array of objects called 'clues'. An object will look like this: {emoji: '😀', reason: 'This ${category} is usually happy.'}
+                The name of the random ${category} you chose should be a key value pair where the key is 'answer' and the value is the name of the ${category}.
+                Here is the entire structure of the object not including values {clues:[{emoji:'',reason:''}],answer:['']}
                 `,
             }
             
@@ -55,23 +55,22 @@ async function callTriviaAI(req, res, category) {
     
 }
 
-
 async function callGuessAI(req, res, guess,answer) {
+    console.log("Guess: "+guess);
+    console.log("Answer: "+answer);
+
+    var correctAnswer = areStringsSimilar(guess,answer);
+    var prompt;
+    if(correctAnswer)
+        prompt = "Tell me how smart and am and say that i guessed the right answer!";
+    else
+        prompt = "Insult my intelligence and tell me how stupid i am for guessing the wrong answer!";
     const completion = await openai.chat.completions.create({
         messages: [
             {
                 role: "system",
-                content: `
-                You are the trivia master for a game we are playing.
-                I will give you a players guess and then the correct answer (split by a colon).
-                If the guess is the same as the answer, you should tell them how smart they are in a clever and cheerful way.
-                If the guess is not the same as the answer, you should tell them how stupid they are in a spiteful and mean way.
-                `
+                content: prompt
             },
-            {
-                role: "user",
-                content: guess.toLowerCase()+":"+answer.toLowerCase()
-            }
         ],
         model: "gpt-3.5-turbo-0125"
     });
@@ -80,6 +79,39 @@ async function callGuessAI(req, res, guess,answer) {
     res.end(JSON.stringify(completion.choices[0].message.content));
 }
 
+function levenshteinDistance(a, b) {
+  const matrix = [];
+
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1,
+          Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1)
+        );
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+}
+
+function areStringsSimilar(a, b, threshold = 0.6) {
+  const distance = levenshteinDistance(a, b);
+  const longestLength = Math.max(a.length, b.length);
+  const similarity = (longestLength - distance) / longestLength;
+  return similarity >= threshold;
+}
 
 function handleUnknownRoute(req, res) {
     res.statusCode = 404;
